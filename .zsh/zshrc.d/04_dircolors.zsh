34d31f1d78ac965f504e74bc13c62406e7e29fc3
Qi Ji <qjqqyy@gmail.com>
2014-10-27 05:34:58 +0800

    solarized dircolors only for supported terminals (simple heuristics but care)

--- .zsh/zshrc.d/04_dircolors.zsh
+++ .zsh/zshrc.d/04_dircolors.zsh
@@ -1,2 +1,9 @@
-# Solarized dircolors
-eval `dircolors ~/.zsh/.dircolors`
+case $TERM in
+  termite|*xterm*|konsole|konsole-256color|rxvt|rxvt-unicode|rxvt-256color|rxvt-unicode-256color|(dt|k|E)term|screen*)
+    # Solarized dircolors for graphical terminals
+    eval `dircolors -b $HOME/.zsh/.dircolors`
+    ;;
+  *)
+    eval `dircolors -b`
+    ;;
+esac
