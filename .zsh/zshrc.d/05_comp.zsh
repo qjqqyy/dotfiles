5a6a3314d0d8532df82486c17bf3e9a842180b9d
Qi Ji <qjqqyy@gmail.com>
2014-10-26 18:19:32 +0800

    refined autocompletes

--- .zsh/zshrc.d/05_comp.zsh
+++ .zsh/zshrc.d/05_comp.zsh
@@ -1,13 +1,82 @@
 autoload -Uz compinit
 compinit
-setopt completealiases
+setopt completealiases completeinword alwaystoend
 #zstyle :compinstall filename "$HOME/.zshrc"
-zstyle ':completion:*:descriptions' format '%U%B%d%b%u' 
-zstyle ':completion:*:warnings' format '%BSorry, no matches for: %d%b'
-zstyle ':completion:*' menu select=6
-zstyle ':completion:*:functions' ignored-patterns '_*'
+#zstyle ':completion:*:descriptions' format '%U%B%d%b%u' 
+#zstyle ':completion:*:warnings' format '%BSorry, no matches for: %d%b'
+#zstyle ':completion:*:functions' ignored-patterns '_*'
+#
+#zstyle ':completion:*:*:kill:*' menu yes select
+#zstyle ':completion:*:kill:*'   force-list always
+#
+#zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
+zstyle ':completion:*:*:*:*:*' menu select=6
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found for %d --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+# Increase the number of errors based on the length of the typed word.
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+# Don't complete unavailable commands.
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+# Array completion element sorting.
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+# Directories
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+zstyle ':completion:*' squeeze-slashes true
+# History
+zstyle ':completion:*:history-words' stop yes
+zstyle ':completion:*:history-words' remove-all-dups yes
+zstyle ':completion:*:history-words' list false
+zstyle ':completion:*:history-words' menu yes
+# Environmental Variables
+zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
+
+# Don't complete uninteresting users...
+zstyle ':completion:*:*:*:users' ignored-patterns \
+adm amanda apache 'avahi*' backup beaglidx bin cacti canna clamav colord \
+daemon Debian-exim dbus distcache dovecot fax ftp games gdm gnats gkrellmd \
+gopher hacluster halt hplip hsqldb ident irc junkbust ldap lightdm list lp \
+'mail*' man messagebus mopidy mysql nagios named netdump news nfsnobody \
+nobody nscd ntp nut nx openvpn operator pcap postfix postgres privoxy proxy \
+pulse pvm quagga radvd rpc rpcuser rpm saned shutdown squid sshd statd sync \
+sys 'systemd-*' uucp uuidd vcsa www-data xfs '_*'
+# ... unless we really want to.
+zstyle '*' single-ignored show
+# Ignore multiple entries.
+zstyle ':completion:*:(rm|kill|diff):*' ignore-line yes
+zstyle ':completion:*:rm:*' file-patterns '*:all-files'
+# Kill
+zstyle ':completion:*:*:*:*:processes' command 'ps -u $USER -o pid,user,comm -w'
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
 zstyle ':completion:*:*:kill:*' menu yes select
-zstyle ':completion:*:kill:*'   force-list always
+zstyle ':completion:*:*:kill:*' force-list always
+zstyle ':completion:*:*:kill:*' insert-ids single
+# Man
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.(^1*)' insert-sections true
 
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+# SH/SCP/RSYNC
+#zstyle ':completion:*:(scp|rsync):*' tag-order 'hosts:-host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+#zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
+#zstyle ':completion:*:ssh:*' tag-order users 'hosts:-host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+#zstyle ':completion:*:ssh:*' group-order hosts-domain hosts-host users hosts-ipaddr
+#zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*.*' loopback localhost
+#zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^*.*' '*@*'
+#zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^<->.<->.<->.<->' '127.0.0.<->'
