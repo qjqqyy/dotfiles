4740c9f3adf379cc741e70ffccd1b71f6eace274
Qi Ji <qjqqyy@gmail.com>
2014-10-28 09:28:58 +0800

    window title is not needed

--- zsh/zshrc.d/10_keys_prompt.zsh
+++ zsh/zshrc.d/10_keys_prompt.zsh
@@ -1,4 +1,4 @@
-autoload -Uz colors vcs_info
+autoload -Uz colors #vcs_info
 colors
 PROMPT="%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%}:%{$fg[yellow]%}%~%{$reset_color%}%# " 
 RPROMPT_="%{$fg[green]%}%* %{$reset_color%}[%{$fg[magenta]%}%?%{$reset_color%}]"
