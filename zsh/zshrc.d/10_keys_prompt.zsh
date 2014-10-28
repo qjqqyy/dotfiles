52ea720d6a05a00c9fe1d17d7e8ab6c8d94d0b0a
Qi Ji <qjqqyy@gmail.com>
2014-10-28 08:33:49 +0800

    fix time on rprompt

--- zsh/zshrc.d/10_keys_prompt.zsh
+++ zsh/zshrc.d/10_keys_prompt.zsh
@@ -1,7 +1,7 @@
 autoload -Uz colors vcs_info
 colors
 PROMPT="%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%}:%{$fg[yellow]%}%~%{$reset_color%}%# " 
-RPROMPT_="%{$fg[green]%}%* [%{$fg[magenta]%}%?%{$reset_color%}]"
+RPROMPT_="%{$fg[green]%}%* %{$reset_color%}[%{$fg[magenta]%}%?%{$reset_color%}]"
 RPROMPT=$RPROMPT_
 bindkey -v
 # create a zkbd compatible hash;
