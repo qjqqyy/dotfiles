9a117639a64c30afac818b32629a90d6464bd138
Qi Ji <qjqqyy@gmail.com>
2014-11-01 22:41:36 +0800

    lc collate bsd

--- zsh/zshrc.d/00_options.zsh
+++ zsh/zshrc.d/00_options.zsh
@@ -9,5 +9,6 @@ KEYTIMEOUT=2
 export EDITOR='vim'
 export VISUAL='vim'
 
-export LC_COLLATE='C'
+# only linux sorts like a bitch
+[[ $OSTYPE =~ bsd ]] || export LC_COLLATE='C'
 export TZ='Asia/Singapore'
