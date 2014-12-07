44e0e88758dd559bd072e1702abeed135320f581
Qi Ji <qjqqyy@gmail.com>
2014-12-07 11:46:01 +0800

    improve detection for terminal-notifier

--- zsh/zshrc.d/94_osxnofity.zsh
+++ zsh/zshrc.d/94_osxnofity.zsh
@@ -1,5 +1,7 @@
-if [[ $OSTYPE =~ darwin && -x =terminal-notifier ]]; then
-    function growl(){ 
-        terminal-notifier -activate com.googlecode.iterm2 -title "iTerm 2" -subtitle $(print -Pn %n@%m:%d) -message "$@ finished" 
-    }
+if [[ $OSTYPE =~ darwin ]]; then
+    if hash terminal-notifier > /dev/null 2>&1; then
+        function growl(){ 
+            terminal-notifier -activate com.googlecode.iterm2 -title "iTerm 2" -subtitle $(print -Pn %n@%m:%d) -message "$@ finished" 
+        }
+    fi
 fi
