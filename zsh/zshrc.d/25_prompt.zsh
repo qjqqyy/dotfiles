a0626850d07bad9e3bec7a5dfef1fc6ee8178ee6
Qi Ji <qjqqyy@gmail.com>
2015-02-15 22:37:31 +0800

    add a space after pwd in prompt

--- zsh/zshrc.d/25_prompt.zsh
+++ zsh/zshrc.d/25_prompt.zsh
@@ -2,9 +2,9 @@
 #autoload -Uz colors #vcs_info
 #colors
 if [[ $UID -eq 0 ]]; then
-    PROMPT="%{$bg_bold[red]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%}:%{$fg[yellow]%}%~%{$reset_color%}%# "
+    PROMPT="%{$bg_bold[red]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%}:%{$fg[yellow]%}%~%{$reset_color%} %# "
 else 
-    PROMPT="%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%}:%{$fg[yellow]%}%~%{$reset_color%}%# "
+    PROMPT="%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[cyan]%}%m%{$reset_color%}:%{$fg[yellow]%}%~%{$reset_color%} %# "
 fi
 RPROMPT_="%{$fg[green]%}%* %{$reset_color%}[%{$fg[magenta]%}%?%{$reset_color%}]"
 RPROMPT=$RPROMPT_
