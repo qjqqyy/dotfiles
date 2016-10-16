3db4b3c3dcfa10548bbc3590f88611a2b573d25f
Qi Ji <qjqqyy@gmail.com>
2016-10-17 00:37:15 +0800

    prefer system zsh-syntax-highlighting

--- zsh/zshrc.d/99_syntax-highlighting.zsh
+++ zsh/zshrc.d/99_syntax-highlighting.zsh
@@ -1,11 +1,8 @@
 # Syntax highlighting
-if  [[ -r ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] 
-then
-  source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-elif [[ -r /usr/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh ]]
-then
-   source /usr/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh
-elif [[ -r /usr/local/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh ]]
-then
-   source /usr/local/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh
+if [[ -r /usr/local/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh ]]; then
+    source /usr/local/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh
+elif [[ -r /usr/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh ]]; then
+    source /usr/share/zsh/plugins/zsh-syntax-highlighting/zyn-syntax-highlighting.zsh
+elif  [[ -r ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+    source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi;
