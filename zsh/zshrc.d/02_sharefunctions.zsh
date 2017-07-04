8e9f649c867a9d68a903deaa0efe64f5d57af1f0
Qi Ji <qjqqyy@gmail.com>
2017-07-04 22:28:46 +0800

    add sharing functions 0x0, sprunge & ghetty add function clipboard for slightly portable clipboard access

---
+++ zsh/zshrc.d/02_sharefunctions.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+# pastes & uploads
+clipboard() {
+  if [[ ! -z $DISPLAY ]]; then
+    if hash pbcopy > /dev/null 2>&1; then
+      clip=pbcopy
+    elif hash xclip > /dev/null 2>&1; then
+      clip=xclip
+    elif hash xsel > /dev/null 2>&1; then
+      clip=xsel
+    else
+      clip=cat
+    fi
+  else
+    clip=cat
+  fi
+  $clip
+}
+sprunge() {
+  cat $* | curl -sSf -F 'sprunge=<-' http://sprunge.us | clipboard
+}
+0x0() {
+  curl -sSf -F"file=@$1" http://0x0.st | clipboard
+}
+autoload -Uz ghetty && alias ghetty='noglob ghetty'
