e1e256df7adf78cf24fbe55231a0a6f7573abcec
Qi Ji <qjqqyy@gmail.com>
2017-08-28 21:36:27 +0800

    refactor aliases + use gnu utils if possible on ancient unix

--- zsh/zshrc.d/01_alias.zsh
+++ zsh/zshrc.d/01_alias.zsh
@@ -1,29 +1,51 @@
 # Aliases
+typeset -A _ls_flags _rm_flags _mv_flags _cp_flags
+_ls_flags[gnu]='-FH --group-directories-first --color=auto'
+_rm_flags[gnu]='-Iv --one-file-system'
+_ls_flags[freebsd]='-FHG'
+_rm_flags[freebsd]='-iv'
+_ls_flags[darwin]='-FHG'
+_rm_flags[darwin]='-iv'
+_ls_flags[compat]='-FH'
+_rm_flags[compat]='-i'
+_mv_flags[modern]='-iv'
+_cp_flags[modern]='-ipv'
+_mv_flags[compat]='-i'
+_cp_flags[compat]='-ip'
+
 case $OSTYPE in
   *gnu|*linux*)
-    alias ls='ls -FH --group-directories-first --color=auto'
-    alias rm='nocorrect noglob rm -Iv --one-file-system'
+    alias ls="ls ${_ls_flags[gnu]}"
+    alias cp="cp ${_cp_flags[modern]}"
+    alias rm="nocorrect noglob rm ${_rm_flags[gnu]}"
+    alias mv="nocorrect noglob mv ${_mv_flags[modern]}"
     ;;
   *darwin*)
-    alias ls='ls -FGH'
-    alias rm='nocorrect noglob rm -iv'
+    alias ls="ls ${_ls_flags[darwin]}"
+    alias cp="cp ${_cp_flags[modern]}"
+    alias rm="nocorrect noglob rm ${_rm_flags[darwin]}"
+    alias mv="nocorrect noglob mv ${_mv_flags[modern]}"
     ;;
   freebsd*)
-    alias ls='ls -FGH'
-    alias rm='nocorrect noglob rm -Ivx'
+    alias ls="ls ${_ls_flags[freebsd]}"
+    alias cp="cp ${_cp_flags[modern]}"
+    alias rm="nocorrect noglob rm ${_rm_flags[freebsd]}"
+    alias mv="nocorrect noglob mv ${_mv_flags[modern]}"
     ;;
   solaris*|openbsd*)
-    alias ls='ls -FH'
-    alias rm='nocorrect noglob rm -i'
+    if hash gls && hash gcp && hash gmv && hash grm > /dev/null 2>&1; then
+      alias ls="gls ${_ls_flags[gnu]}"
+      alias cp="gcp ${_cp_flags[modern]}"
+      alias rm="nocorrect noglob grm ${_rm_flags[gnu]}"
+      alias mv="nocorrect noglob gmv ${_mv_flags[modern]}"
+    else
+      alias ls="ls ${_ls_flags[compat]}"
+      alias cp="cp ${_cp_flags[compat]}"
+      alias rm="nocorrect noglob rm ${_rm_flags[compat]}"
+      alias mv="nocorrect noglob mv ${_mv_flags[compat]}"
+    fi
     ;;
 esac
-# # be paranoid
-if [[ $OSTYPE =~ (openbsd|solaris) ]]; then
-  alias mv='nocorrect noglob mv -i'
-else
-  alias mv='nocorrect noglob mv -iv'
-fi
-alias cp='cp -ip'
 alias ll='ls -l'
 alias la='ls -A'
 alias lla='ll -A'
@@ -31,15 +53,19 @@ alias lh='ll -h'
 alias lah='lla -h'
 alias :q='exit'
 alias /quit='exit'
-if [[ $OSTYPE =~ freebsd ]]; then
-  alias grep='bsdgrep -i --color=auto -d skip'
-elif [[ $OSTYPE =~ (openbsd|solaris) ]]; then
-  alias grep='grep -i'
+if [[ $OSTYPE =~ (openbsd|solaris) ]]; then
+  if hash ggrep > /dev/null 2>&1; then
+    alias grep='ggrep -i --color=auto -d skip'
+  else
+    alias grep='grep -i'
+  fi
 else
   alias grep='grep -i --color=auto -d skip'
 fi
 alias df='df -h'
-[[ $OSTYPE =~ linux ]] && alias free='free -m'  # show sizes in MB
+[[ $OSTYPE =~ linux ]] && alias free='free -m'
 alias git='nocorrect noglob git'
 # force mouse off
 alias mousereset=" printf '\033[?1002l'"
+
+unset _ls_flags _rm_flags _mv_flags _cp_flags
