2367eb8863cce39ec702f95e19d799bab1d5e911
Qi Ji <qjqqyy@gmail.com>
2018-07-07 05:13:32 +0800

    move java nonstandard flags to environment

--- zsh/zshrc.d/03_kattis.zsh
+++ zsh/zshrc.d/03_kattis.zsh
@@ -3,5 +3,6 @@ export GHCRTS='-M1024m -K8m'
 
 alias katcc='cc -lm -g -O2 -Wall -Wextra -pedantic -std=gnu99'
 
+export _JAVA_OPTIONS='-XX:+UseSerialGC -Xss64m -Xms1024m -Xmx1024m'
 alias javac='javac -encoding UTF-8 -sourcepath . -d .'
-alias java='java -XX:+UseSerialGC -Xss64m -Xms1024m -Xmx1024m -Dfile.encoding=UTF-8 -cp .'
+alias java='java -Dfile.encoding=UTF-8 -cp .'
