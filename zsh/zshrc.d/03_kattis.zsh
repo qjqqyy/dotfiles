1e1d9c453bb331c36af539f2d7b92022153624b3
Qi Ji <qjqqyy@gmail.com>
2018-08-12 04:58:50 +0800

    c++ aliases && vim stuff yay

--- zsh/zshrc.d/03_kattis.zsh
+++ zsh/zshrc.d/03_kattis.zsh
@@ -6,3 +6,5 @@ alias katcc='cc -lm -g -O2 -Wall -Wextra -pedantic -std=gnu99'
 export _JAVA_OPTIONS='-XX:+UseSerialGC -Xss64m -Xms1024m -Xmx1024m'
 alias javac='javac -encoding UTF-8 -sourcepath . -d .'
 alias java='java -Dfile.encoding=UTF-8 -cp .'
+
+alias katcx='c++ -g -O2 -Wall -Wextra -pedantic -std=gnu++14'
