0f3bce7a52d8a2f5ed63b712015625096941d40c
qjqqyy <qjqqyy@gmail.com>
2019-01-25 20:31:27 +0800

    alias compilers only if they're actually present

--- zsh/zshrc.d/03_kattis.zsh
+++ zsh/zshrc.d/03_kattis.zsh
@@ -1,10 +1,13 @@
-alias katghc='ghc -O2 -ferror-spans -threaded -rtsopts'
-export GHCRTS='-M1024m -K8m'
+if hash ghc > /dev/null 2>&1; then
+  alias katghc='ghc -O2 -ferror-spans -threaded -rtsopts'
+  export GHCRTS='-M1024m -K8m'
+fi
+
+if hash javac > /dev/null 2>&1; then
+  export _JAVA_OPTIONS='-XX:+UseSerialGC -Xss64m -Xms1024m -Xmx1024m'
+  alias javac='javac -encoding UTF-8 -sourcepath . -d .'
+  alias java='java -Dfile.encoding=UTF-8 -cp .'
+fi
 
 alias katcc='cc -lm -g -O2 -Wall -Wextra -pedantic -std=gnu99'
-
-export _JAVA_OPTIONS='-XX:+UseSerialGC -Xss64m -Xms1024m -Xmx1024m'
-alias javac='javac -encoding UTF-8 -sourcepath . -d .'
-alias java='java -Dfile.encoding=UTF-8 -cp .'
-
 alias katcx='c++ -g -O2 -Wall -Wextra -pedantic -std=gnu++14'
