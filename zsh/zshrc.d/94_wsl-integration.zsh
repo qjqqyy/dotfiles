4a60ece2f77f7e8aeb6a5f753d25aab94689a70c
Qi Ji <qjqqyy@gmail.com>
2019-05-18 03:17:09 +0800

    generalise more WSL stuff

---
+++ zsh/zshrc.d/94_wsl-integration.zsh
@@ -0,0 +1,17 @@
+if (( $+commands[powershell.exe] )); then
+  # integrates with ssh-agent-wsl
+  # path to executable should be set beforehand as $_ssh_agent_wsl
+  if [[ ! -z $_ssh_agent_wsl ]]; then
+    eval $("$_ssh_agent_wsl" | sed '/echo/d' )
+    unset _ssh_agent_wsl # _ssh_agent_sock
+  fi
+
+  _windows_username=$(powershell.exe '$env:UserName')
+  # dir bookmark
+  hash -d -- winhome="/mnt/c/Users/${_windows_username//$'\r'}"
+
+  # suffix alias for .bat files
+  alias -s bat='cmd.exe /c'
+
+  unset _windows_username
+fi
