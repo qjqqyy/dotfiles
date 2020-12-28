83bc254bda6464398cdd53d96ed733dafc16c9d8
Qi Ji <qjqqyy@gmail.com>
2020-12-28 12:23:12 +0800

    change font size for new monitor

--- xmonad-xmobar/xmobar-fleur.hs
+++ xmonad-xmobar/xmobar-fleur.hs
@@ -4,6 +4,6 @@ import Xmobar
 
 main :: IO ()
 main = mkMain (MachineSpecificCrap [] []) defaultConfig
-    { font = "xft:Fantasque Sans Mono:pixelsize=16"
-    , additionalFonts = [ "xft:Noto Sans SC:pixelsize=14" ]
+    { font = "xft:Fantasque Sans Mono:pixelsize=24"
+    , additionalFonts = [ "xft:Noto Sans SC:pixelsize=20" ]
     }
