4b575b1aa2bce7f3d1fc1027af61d3cc89d8e8af
Qi Ji <qjqqyy@gmail.com>
2021-10-29 02:17:49 +0800

    homebrew on arm64 installs to /opt/homebrew

--- zsh/zshrc.d/99_syntax-highlighting.zsh
+++ zsh/zshrc.d/99_syntax-highlighting.zsh
@@ -5,6 +5,8 @@ elif [[ -r /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlightin
   source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 elif [[ -r /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
   source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [[ -r /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 elif [[ -r /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
   source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 elif [[ -r $HOME/.local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
