29d6275364fbce574ee6d7dad9adb9baa427a0c6
Qi Ji <qjqqyy@gmail.com>
2021-10-31 10:27:15 +0800

    zbell iterm2 notification

--- zsh/zshrc.d/94_osxnofity.zsh
+++
@@ -1,7 +0,0 @@
-if [[ $OSTYPE =~ darwin ]]; then
-  if (( $+commands[terminal-notifier] )); then
-    function growl(){
-      terminal-notifier -activate com.googlecode.iterm2 -title "iTerm 2" -subtitle $(print -Pn %n@%m:%d) -message "$@ finished" -timeout 5 2>/dev/null &|
-    }
-  fi
-fi
