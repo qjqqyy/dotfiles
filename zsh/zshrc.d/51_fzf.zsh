68bfc827b04a14e802057bf60643135e534455a0
Qi Ji <qjqqyy@gmail.com>
2025-02-02 22:56:50 +0800

    _

--- zsh/zshrc.d/51_fzf.zsh
+++ zsh/zshrc.d/51_fzf.zsh
@@ -1,9 +1,11 @@
-[[ -n "$TMUX" ]] && export FZF_TMUX=1
+if (( $+commands[fzf] )); then
+  [[ -n "$TMUX" ]] && export FZF_TMUX=1
 
-# I don't actually like fuzzy finding
-export FZF_DEFAULT_OPTS="--exact --layout=reverse-list --color=16,bg+:0,fg+:15,gutter:10,prompt:2,border:11,pointer:5"
-export FZF_DEFAULT_COMMAND='fd --hidden --follow'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
+  # I don't actually like fuzzy finding
+  export FZF_DEFAULT_OPTS="--exact --layout=reverse-list --color=16,bg+:0,fg+:15,gutter:10,prompt:2,border:11,pointer:5"
+  export FZF_DEFAULT_COMMAND='fd --hidden --follow'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
 
-eval "$( fzf --zsh )"
+  eval "$( fzf --zsh )"
+fi
