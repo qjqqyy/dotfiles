9a09dcd3b3069ce83dea3eda51974ba9016674f7
Qi Ji <qjqqyy@gmail.com>
2025-04-22 20:42:47 +0800

    .

--- zsh/zshrc.d/30_fzf.zsh
+++ zsh/zshrc.d/30_fzf.zsh
@@ -1,11 +1,19 @@
 if (( $+commands[fzf] )); then
   [[ -n "$TMUX" ]] && export FZF_TMUX=1
-
-  # I don't actually like fuzzy finding
+  
   export FZF_DEFAULT_OPTS="--exact --layout=reverse-list --color=16,bg+:0,fg+:15,gutter:10,prompt:2,border:11,pointer:5"
-  export FZF_DEFAULT_COMMAND='fd --hidden --follow'
-  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
 
-  eval "$( fzf --zsh )"
+  local fdfind
+  if (( $+commands[fd] )); then
+    fdfind="fd"
+  elif (( $+commands[fdfind] )); then
+    fdfind="fdfind"
+  fi
+
+  if [[ -n "$fdfind" ]]; then
+    export FZF_DEFAULT_COMMAND="$fdfind --hidden --follow"
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+    export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
+  fi
+  ( fzf --zsh 2>/dev/null || cat /usr/share/doc/fzf/examples/key-bindings.zsh /usr/share/doc/fzf/examples/completion.zsh ) | source /dev/fd/0
 fi
